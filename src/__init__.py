62ab648978e67549e955c817e8ad49a4117491ba
Andrew Wang <siweiw9@gmail.com>
2026-08-25 22:15:25 -0700

    unit testing for palette clustering

--- src/__init__.py
+++ src/__init__.py
@@ -5,7 +5,8 @@ Copyright 2026. Andrew Wang
 """
 # pylint: disable=useless-import-alias
 
-from .elbow import cluster_count as cluster_count
+from .elbow import cluster_count as cluster_count, \
+    MIN_CLUSTERS as MIN_CLUSTERS, MAX_CLUSTERS as MAX_CLUSTERS
 from .lab import get_pixels as get_pixels
 from .cluster import compute_palette as compute_palette
 from .display import render_page as render_page
