382ba8d5907d78319d32d81be02c757775b7f1ae
Andrew Wang <siweiw9@gmail.com>
2026-09-03 13:23:08 -0700

    refactor free floating functions into classes

--- test/test_cluster.py
+++ test/test_cluster.py
@@ -8,7 +8,7 @@ from pathlib import Path
 import pandas as pd
 import pytest
 
-from src import MAX_CLUSTERS, MIN_CLUSTERS, compute_palette, get_pixels
+from src import MAX_ELBOW_CLUSTERS, MIN_ELBOW_CLUSTERS, Clustering, Pixels
 
 RANDOM_SEED = 42
 
@@ -20,7 +20,7 @@ def _get_clusters() -> list[tuple[Path, pd.DataFrame]]:
     tests = []
     for img in images:
         img_path = Path("images") / f"{img}.jpg"
-        for clusters in range(MIN_CLUSTERS, MAX_CLUSTERS + 1):
+        for clusters in range(MIN_ELBOW_CLUSTERS, MAX_ELBOW_CLUSTERS + 1):
             sub_df = df[(df.image == img) & (df.clusters == clusters)]
             sub_df = sub_df.drop(columns=["image", "clusters"])
             tests.append((img_path, sub_df))
@@ -30,8 +30,9 @@ def _get_clusters() -> list[tuple[Path, pd.DataFrame]]:
 @pytest.mark.parametrize(("image_path", "expected_df"), _get_clusters())
 def test_cluster(image_path: Path, expected_df: pd.DataFrame) -> None:
     """Test clustering for images."""
-    pixels = get_pixels(image_path)
-    actual_df = compute_palette(pixels, expected_df.shape[0], rand_state=RANDOM_SEED)
+    pixels = Pixels(image_path)
+    clustering = Clustering(pixels.as_ok_lab(), RANDOM_SEED)
+    actual_df = clustering.compute_palette(expected_df.shape[0])
     pd.testing.assert_frame_equal(
         expected_df.sort_values(by="hex").reset_index(drop=True),
         actual_df.sort_values(by="hex").reset_index(drop=True),
