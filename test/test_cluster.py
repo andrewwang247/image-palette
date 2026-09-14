c1c6af05e078ae1c94524aacd6e3570170134adc
Andrew Wang <siweiw9@gmail.com>
2026-09-13 17:14:47 -0700

    avoid duplicated reconstruct with conftest

--- test/test_cluster.py
+++ test/test_cluster.py
@@ -3,36 +3,22 @@
 Copyright 2026. Andrew Wang.
 """
 
-from pathlib import Path
+from typing import TYPE_CHECKING
 
 import pandas as pd
 import pytest
 
-from src import MAX_ELBOW_CLUSTERS, MIN_ELBOW_CLUSTERS, Clustering, FloatGrid, Pixels
+from .conftest import CLUSTER_RNG
+
+if TYPE_CHECKING:
+    from .conftest import Solution
 
 
-def _get_clusters() -> list[tuple[FloatGrid, pd.DataFrame]]:
-    """Load images and cluster solutions."""
-    df = pd.read_csv("test/cluster.csv")
-    images = df.image.unique()
-    tests = []
-    for img in images:
-        img_path = Path("images") / f"{img}.jpg"
-        img_df = df[df.image == img].drop(columns=["image"])
-        pixels = Pixels(img_path).as_ok_lab()
-        for clusters in range(MIN_ELBOW_CLUSTERS, MAX_ELBOW_CLUSTERS + 1):
-            sub_df = img_df[img_df.clusters == clusters].drop(columns=["clusters"])
-            tests.append((pixels, sub_df))
-    return tests
-
-
-@pytest.mark.parametrize(("pixels", "expected_df"), _get_clusters())
-def test_cluster(
-    random_seed: int, pixels: FloatGrid, expected_df: pd.DataFrame
-) -> None:
-    """Test clustering for images."""
-    clustering = Clustering(pixels, random_seed)
-    actual_df = clustering.compute_palette(expected_df.shape[0])
+@pytest.mark.parametrize("clusters", CLUSTER_RNG)
+def test_cluster(solution: Solution, clusters: int) -> None:
+    """Validate clustering produces correct centroids."""
+    actual_df = solution.clustering.compute_palette(clusters)
+    expected_df = solution.palette[clusters]
     pd.testing.assert_frame_equal(
         actual_df.sort_values(by="hex").reset_index(drop=True),
         expected_df.sort_values(by="hex").reset_index(drop=True),
--- test/test_elbow.py
+++ test/test_elbow.py
@@ -1,30 +1,15 @@
-"""Test elbow method for selecting number of clusters.
+"""Test elbow computation.
 
 Copyright 2026. Andrew Wang.
 """
 
-from json import load
-from pathlib import Path
+from typing import TYPE_CHECKING
 
-import pytest
-
-from src import Clustering, FloatGrid, Pixels
+if TYPE_CHECKING:
+    from .conftest import Solution
 
 
-def _get_elbows() -> list[tuple[FloatGrid, int]]:
-    """Load images and elbow solutions."""
-    with Path("test/elbow.json").open(encoding="UTF-8") as fp:
-        elbows: dict[str, int] = load(fp)
-    params: list[tuple[FloatGrid, int]] = []
-    for fname, expected_elbow in elbows.items():
-        pixels = Pixels(Path(fname))
-        params.append((pixels.as_ok_lab(), expected_elbow))
-    return params
-
-
-@pytest.mark.parametrize(("pixels", "expected_clusters"), _get_elbows())
-def test_elbow(random_seed: int, pixels: FloatGrid, expected_clusters: int) -> None:
-    """Test elbow method for images."""
-    clustering = Clustering(pixels, random_seed)
-    actual_clusters = clustering.cluster_count()
-    assert actual_clusters == expected_clusters
+def test_elbow(solution: Solution) -> None:
+    """Validate that elbow method produces correct value."""
+    actual_clusters = solution.clustering.cluster_count()
+    assert actual_clusters == solution.elbow
