d4fa88e62a5a61e92f21a1d7390c9dcd96c2e8da
Andrew Wang <siweiw9@gmail.com>
2026-09-13 22:29:49 -0700

    consistent logging and header sections

--- src/display.py
+++ src/display.py
@@ -7,16 +7,16 @@ import logging
 from pathlib import Path
 from typing import TYPE_CHECKING, Any
 
+from jinja2 import Environment, FileSystemLoader
+
 if TYPE_CHECKING:
     from collections.abc import Iterable
 
     import pandas as pd
-from jinja2 import Environment, FileSystemLoader
-
-TEMPLATE_DIR = "templates"
-TEMPLATE_NAME = "palette.jinja"
 
 logger = logging.getLogger(__name__)
+
+_TEMPLATE_DIR = Path("templates")
 
 
 class Display:
@@ -24,18 +24,18 @@ class Display:
 
     def __init__(self) -> None:
         """Load Jinja template and store output path."""
-        current_script_dir = Path(__file__).resolve().parent
-        root_dir = current_script_dir.parent
-        logger.info("Loading Jinja template from file system")
-        jinja_env = Environment(
-            loader=FileSystemLoader(root_dir / TEMPLATE_DIR), autoescape=True
-        )
-        self.template = jinja_env.get_template(TEMPLATE_NAME)
+        logger.info("Loading Jinja environment from %s directory", _TEMPLATE_DIR)
+        jinja_env = Environment(loader=FileSystemLoader(_TEMPLATE_DIR), autoescape=True)
+
+        template_path = _TEMPLATE_DIR / "palette.jinja"
+        assert template_path.is_file(), f"No template at {template_path}"
+        logger.info("Retrieving Jinja template at %s", template_path)
+        self.template = jinja_env.get_template(template_path.name)
 
     def render_page(self, img_path: Path, df: pd.DataFrame) -> str:
         """Display palette in a webpage."""
         assert img_path.is_file(), f"Path {img_path} is not a file."
-        logger.info("Rendering template with palette data")
+        logger.info("Rendering template with palette data for %s", img_path)
         return self.template.render(
             filename=img_path.name,
             filepath=img_path,
--- src/pixel.py
+++ src/pixel.py
@@ -6,17 +6,17 @@ Copyright 2026. Andrew Wang.
 import logging
 from typing import TYPE_CHECKING, Any
 
-if TYPE_CHECKING:
-    from pathlib import Path
-
 import numpy as np
 from colour.models import Oklab_to_XYZ, XYZ_to_Oklab, XYZ_to_sRGB, sRGB_to_XYZ
 from PIL import Image
 
-SRGB_SCALING = 255.0
+if TYPE_CHECKING:
+    from pathlib import Path
+
 logger = logging.getLogger(__name__)
 
-MAX_DIM = (1024, 1024)
+_MAX_DIM = (1024, 1024)
+_SRGB_SCALING = 255
 
 type IntGrid = np.ndarray[tuple[int, int], np.dtype[np.int_]]
 type FloatGrid = np.ndarray[tuple[int, int], np.dtype[np.floating[Any]]]
@@ -31,12 +31,12 @@ class Pixels:
         assert fpath.is_file(), f"Path {fpath} is not a file."
         self.img = Image.open(fpath)
         logger.info("Before resampling: %d x %d px", self.img.width, self.img.height)
-        self.img.thumbnail(MAX_DIM, Image.Resampling.LANCZOS)
+        self.img.thumbnail(_MAX_DIM, Image.Resampling.LANCZOS)
         logger.info("After resampling: %d x %d px", self.img.width, self.img.height)
 
     def as_ok_lab(self) -> FloatGrid:
         """Convert Image to OkLAB format."""
-        img_arr = np.array(self.img.convert("RGB")) / SRGB_SCALING
+        img_arr = np.array(self.img.convert("RGB")) / _SRGB_SCALING
         logger.info("Converting from sRGB to CIE XYZ")
         img_xyz = sRGB_to_XYZ(img_arr)
         logger.info("Converting from CIE XYZ to Oklab")
@@ -50,4 +50,4 @@ class Pixels:
         xyz_arr = Oklab_to_XYZ(lab_arr)
         logger.info("Converting from CIE XYZ to sRGB")
         rgb_arr = XYZ_to_sRGB(xyz_arr)
-        return np.round(SRGB_SCALING * rgb_arr).astype(np.int_)
+        return np.round(_SRGB_SCALING * rgb_arr).astype(np.int_)
